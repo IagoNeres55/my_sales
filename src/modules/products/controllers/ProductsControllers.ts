434d0173e91ccf1b4bf6b2921f7cef8d1cea43c1
IagoNeres55 <iagoneres90@gmail.com>
2024-12-10 23:29:06 -0300

    feat: finalized controller

--- src/modules/products/controllers/ProductsControllers.ts
+++ src/modules/products/controllers/ProductsControllers.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express'
 import ListProdutcService from '../services/ListProductService'
 import ShowProductService from '../services/ShowProductService'
+import CreateProductService from '../services/CreateProductService'
+import DeleteProductService from '../services/DeleteProductService'
 
 export default class {
-  public async index(request: Request, response: Response): Promise<Response> {
+
+  public async index(_: Request, response: Response): Promise<Response> {
     const listProductsService = new ListProdutcService()
     const products = await listProductsService.execute()
     return response.json(products)
@@ -14,5 +17,24 @@ export default class {
     const showProductsService = new ShowProductService()
     const products = await showProductsService.execute({ id })
     return response.json(products)
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body
+    const createProductService = new CreateProductService()
+    const product = await createProductService.execute({
+      name,
+      price,
+      quantity,
+    })
+    return response.json(product)
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params
+    const deleteProductService = new DeleteProductService()
+    await deleteProductService.execute({ id })
+
+    return response.status(204).send({})
   }
 }
