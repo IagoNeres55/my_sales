98af722f681725cad16d7320933d6e8bd2ca4909
IagoNeres55 <iagoneres90@gmail.com>
2024-12-16 20:31:14 -0300

    feat: create user service

---
+++ src/modules/users/services/CreateUsersService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/erros/AppError'
+import { usersRepositories } from '../database/repositories/UserRepositories'
+import { User } from '../database/entities/User'
+import { hash } from 'bcrypt'
+
+interface ICreateUser {
+  name: string
+  email: string
+  password: string
+}
+
+export default class CreateUserService {
+  async execute({ name, email, password }: ICreateUser): Promise<User> {
+    const emailExists = await usersRepositories.findByEmail(email)
+
+    if (emailExists) {
+      throw new AppError('Email já está em uso', 409)
+    }
+
+    const hashedPassword = await hash(password, 10)
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    await usersRepositories.save(user)
+
+    return user
+  }
+}
