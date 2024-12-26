d5ca8658a978ffe146c109c183f5f29405c48a8c
IagoNeres55 <iagoneres90@gmail.com>
2024-12-26 10:45:05 -0300

    feat: service sendforgot password

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/erros/AppError'
+import { usersRepositories } from '../database/repositories/UserRepositories'
+import { UserTokensRepositories } from '../database/repositories/UserTokensRepositories'
+
+interface IForgotPassword {
+  email: string
+}
+
+export default class SendForgotPasswordEmailService {
+  async execute({ email }: IForgotPassword): Promise<void> {
+    const user = await usersRepositories.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('Usuário não existe', 404)
+    }
+
+    const token = await UserTokensRepositories.generate(user.id)
+    console.log(token)
+  }
+}
