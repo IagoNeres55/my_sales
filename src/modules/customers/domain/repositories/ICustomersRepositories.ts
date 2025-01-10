edefdd224e09f630b29e921d3d156213160ed1b3
IagoNeres55 <iagoneres90@gmail.com>
2025-01-10 19:13:29 -0300

    refactor: implamentation solid to repositories customers

---
+++ src/modules/customers/domain/repositories/ICustomersRepositories.ts
@@ -0,0 +1,18 @@
+import { ICreateCustomer } from '../models/ICreateCustomer'
+import { ICustomer } from '../models/ICustomer'
+
+export interface Pagination {
+  take: number
+  skip: number
+}
+
+// criando um contrato de dominio
+export interface ICustomersRepository {
+  create(data: ICreateCustomer): Promise<ICustomer>
+  save(customer: ICreateCustomer): Promise<ICustomer>
+  remove(customer: ICustomer): Promise<void>
+  findByEmail(email: string): Promise<ICustomer | null>
+  findById(id: number): Promise<ICustomer | null>
+  findAndCount(pagination: Pagination): Promise<[ICustomer[], number]>
+  findByName(name: string): Promise<ICustomer | null>
+}
--- src/modules/customers/infra/database/repositories/CustomersRepositories.ts
+++ src/modules/customers/infra/database/repositories/CustomersRepositories.ts
@@ -1,22 +1,80 @@
 import { AppDataSource } from '@shared/infra/typeorm/data-source'
 import { Customers } from '../entities/Customers'
+import {
+  ICustomersRepository,
+  Pagination,
+} from '@modules/customers/domain/repositories/ICustomersRepositories'
+import { Repository } from 'typeorm'
+import { ICustomer } from '@modules/customers/domain/models/ICustomer'
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer'
 // import { Like } from 'typeorm'
 
-export const customersRepositories = AppDataSource.getRepository(
-  Customers,
-).extend({
-  async findById(id: number): Promise<Customers | null> {
-    return this.findOneBy({ id })
-  },
+export default class customersRepository implements ICustomersRepository {
+  private ormRepository: Repository<Customers>
 
-  async findByName(name: string): Promise<Customers | null> {
-    return this.findOneBy({ name })
-  },
+  constructor() {
+    this.ormRepository = AppDataSource.getRepository(Customers)
+  }
 
-  async findByEmail(email: string): Promise<Customers | null> {
-    return this.findOneBy({ email })
-  },
-})
+  async create({ name, email }: ICreateCustomer): Promise<ICustomer> {
+    const customer = this.ormRepository.create({ name, email })
+    await this.ormRepository.save(customer)
+    return customer
+  }
+
+  async save(customer: ICustomer): Promise<ICustomer> {
+    await this.ormRepository.save(customer)
+    return customer
+  }
+
+  async remove(customer: ICustomer): Promise<void> {
+    await this.ormRepository.remove(customer)
+    return
+  }
+
+  async findByEmail(email: string): Promise<ICustomer | null> {
+    const customer = await this.ormRepository.findOneBy({ email })
+    return customer
+  }
+  async findById(id: number): Promise<ICustomer | null> {
+    const customer = await this.ormRepository.findOneBy({ id })
+    return customer
+  }
+
+  async findAndCount({
+    take,
+    skip,
+  }: Pagination): Promise<[ICustomer[], number]> {
+    const [customer, total] = await this.ormRepository.findAndCount({
+      take,
+      skip,
+    })
+
+    return [customer, total]
+  }
+
+  async findByName(name: string): Promise<ICustomer | null> {
+    const customer = await this.ormRepository.findOneBy({ name })
+    return customer
+  }
+}
+
+// export const customersRepositories = AppDataSource.getRepository(
+//   Customers,
+// ).extend({
+//   async findById(id: number): Promise<Customers | null> {
+//     return this.findOneBy({ id })
+//   },
+
+//   async findByName(name: string): Promise<Customers | null> {
+//     return this.findOneBy({ name })
+//   },
+
+//   async findByEmail(email: string): Promise<Customers | null> {
+//     return this.findOneBy({ email })
+//   },
+// })
+
 // caso precise buscar varios dadoss
 
 // async findAllByName(name: string): Promise<Customers[]> {
