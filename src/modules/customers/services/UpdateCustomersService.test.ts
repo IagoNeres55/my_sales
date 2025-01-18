837b5638972a8d8ce9304cbe9998c7427c90ff62
IagoNeres55 <iagoneres90@gmail.com>
2025-01-18 16:03:31 -0300

    fix: update test

--- src/modules/customers/services/UpdateCustomersService.test.ts
+++ src/modules/customers/services/UpdateCustomersService.test.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/erros/AppError'
 import { customerMock } from '../domain/factories/customerFactory'
 import FakeCustomersRepositories from '../domain/repositories/fakes/FakeCustomersRepositories'
 import { UpdateCustomersService } from './UpdateCustomersService'
@@ -5,14 +6,14 @@ import { UpdateCustomersService } from './UpdateCustomersService'
 let fakeCustomersRepositories: FakeCustomersRepositories
 let updateCustomer: UpdateCustomersService
 describe('UpdateCustomersService', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     fakeCustomersRepositories = new FakeCustomersRepositories()
     updateCustomer = new UpdateCustomersService(fakeCustomersRepositories)
+
+    await fakeCustomersRepositories.create(customerMock)
   })
 
   test('shold be able to update a customer', async () => {
-    const customer = await fakeCustomersRepositories.create(customerMock)
-
     await expect(
       updateCustomer.execute({
         id: 1,
@@ -24,5 +25,30 @@ describe('UpdateCustomersService', () => {
       name: 'Iago Neres',
       email: 'iagoneres90@gmail.com',
     })
+  })
+
+  test('shold be able a error for update emails equals', async () => {
+    const customer = await fakeCustomersRepositories.create({
+      name: 'iago neres',
+      email: 'teste@gmail.com',
+    })
+    console.log(customer)
+    await expect(
+      updateCustomer.execute({
+        id: 1,
+        name: 'Iago Neres teste',
+        email: 'teste@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  test('shold be able a error for update id if not exist', async () => {
+    await expect(
+      updateCustomer.execute({
+        id: 8,
+        name: 'teste teste',
+        email: 'teste@teste.com.br',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
