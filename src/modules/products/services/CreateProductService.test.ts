559880285924f0250cd4d9aba33dd5875b95d266
IagoNeres55 <iagoneres90@gmail.com>
2025-01-22 10:12:14 -0300

    fix: implemente teste mock product

--- src/modules/products/services/CreateProductService.test.ts
+++ src/modules/products/services/CreateProductService.test.ts
@@ -1,9 +1,16 @@
+import AppError from '@shared/erros/AppError'
 import { createProductMock } from '../domain/factories/productFactory'
 import FakeProductsRepositories from '../domain/repositories/fakes/FakeProductsRepositories'
 import CreateProductService from './CreateProductService'
 
 let fakeProductsRepositories: FakeProductsRepositories
 let createProduct: CreateProductService
+
+jest.mock('@shared/cache/RedisCache', () => {
+  return jest.fn().mockImplementation(() => ({
+    invalidade: jest.fn().mockResolvedValue(undefined), // Mock do método invalidate
+  }))
+})
 describe('CreateProductService', () => {
   beforeEach(() => {
     fakeProductsRepositories = new FakeProductsRepositories()
@@ -12,6 +19,12 @@ describe('CreateProductService', () => {
 
   test('Should be able create new product', async () => {
     const product = await createProduct.execute(createProductMock)
-    expect(product.name).toHaveProperty('maça')
+    expect(product.name).toBe('maça')
+  })
+  test('Should be able error product exists', async () => {
+    await createProduct.execute(createProductMock)
+    await expect(
+      createProduct.execute(createProductMock),
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
