5a79fd648e0d578ea7b44325de2c43785d7c0ab5
IagoNeres55 <iagoneres90@gmail.com>
2025-02-11 22:56:19 -0300

    explicação @types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,9 @@
+// O que ele faz?
+
+// Ele adiciona um campo user do tipo { id: string }
+// dentro da Request do Express. Isso é útil quando você
+// trabalha com autenticação e deseja adicionar informações
+// do usuário autenticado ao objeto req de forma tipada.
 declare namespace Express {
   export interface Request {
     user: {
